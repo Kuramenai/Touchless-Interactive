8f1b1cbc0c8060dd72072a76618678e1d4be9724
Kuramenai <marcelynsteeve@gmail.com>
2023-02-28 11:41:49 +0800

    Create a class for the handdetector

--- hand-tracking-module.py
+++ hand-tracking-module.py
@@ -2,56 +2,80 @@ import cv2
 import mediapipe as mp
 import time
 
-previousTime, currentTime = 0, 0
 
+class HandDetector():
+  def __init__(self, mode=False, max_num_hands=2, model_complexity = 1,
+               min_detection_confidence=0.5, min_tracking_confidence=0.5):
+    self.mode = mode
+    self.max_num_hands = max_num_hands
+    self.model_complexity = model_complexity
+    self.min_detection_confidence = min_detection_confidence
+    self.min_tracking_confidence = min_tracking_confidence
 
-cap = cv2.VideoCapture(0)
+    #mediapipe object for hand detection
+    self.mpHands = mp.solutions.hands
+    self.hands = self.mpHands.Hands(self.mode, self.max_num_hands,  self.model_complexity,
+                                    self.min_detection_confidence, self.min_tracking_confidence)
 
-#mediapipe object for hand detection
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
+    #mediapipe object for drawing landmarks
+    self.mpDraw = mp.solutions.drawing_utils
 
-#mediapipe object for drawing landmarks
-mpDraw = mp.solutions.drawing_utils
+  def find_hands(self, frame, draw=True):
 
+    frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = self.hands.process(frameRGB)
 
-if not cap.isOpened():
-  print("Cannot open camera")
-  exit()
+    if results.multi_hand_landmarks:
+      for handLms in results.multi_hand_landmarks:
+        for id, lm in enumerate(handLms.landmark):
+          h, w, c = frame.shape
+          cx, cy = int(lm.x * w), int(lm.y * h)
+          #print(id, cx, cy)
 
-while True:
-  success, frame =cap.read()
+          if id == 0:
+            cv2.circle(frame, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+        if draw:
+          self.mpDraw.draw_landmarks(frame, handLms, self.mpHands.HAND_CONNECTIONS)
 
-  if not success:
-    print("Can't receive frame. Exiting...")
-    break
+    
+    return frame
+  
 
-  frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-  results = hands.process(frameRGB)
-  if results.multi_hand_landmarks:
-    for handLms in results.multi_hand_landmarks:
-      for id, lm in enumerate(handLms.landmark):
-        h, w, c = frame.shape
-        cx, cy = int(lm.x * w), int(lm.y * h)
-        print(id, cx, cy)
+def main():
 
-        if id == 0:
-          cv2.circle(frame, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+  previous_time, current_time = 0, 0
 
-      mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
+  cap = cv2.VideoCapture(0)
+  Detector = HandDetector()
 
-  #Calculate the frame rate
-  currentTime = time.time()
-  fps = 1/(currentTime - previousTime)
-  previousTime = currentTime
+  if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
 
-  #Display the fps value on the screen
-  cv2.putText(frame, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+  while True:
+    success, frame =cap.read()
 
-  cv2.imshow("Frame", frame)
+    if not success:
+      print("Can't receive frame. Exiting...")
+      break
 
-  if cv2.waitKey(1) == ord('q'):
-    break
+    frame = Detector.find_hands(frame)
 
-cap.release()
-cv2.destroyAllWindows()
+    #Calculate the frame rate
+    current_time = time.time()
+    fps = 1/(current_time - previous_time)
+    previous_time = current_time
+
+    #Display the fps value on the screen
+    cv2.putText(frame, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+    cv2.imshow("Frame", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+      break
+
+  cap.release()
+  cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+  main()
