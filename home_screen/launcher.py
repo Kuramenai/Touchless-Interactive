ee1fe9389097895e5b559b55b5f36b8499e09f3a
Kuramenai <marcelynsteeve@gmail.com>
2023-03-15 22:13:28 +0800

    Print the current selection on the screen using a label

--- home_screen/launcher.py
+++ home_screen/launcher.py
@@ -17,30 +17,41 @@ class HomeScreen(QMainWindow):
     def __initVal(self):
         self.__welcomeMessage = QLabel()
         self.__homeMenu = HomeMenu()
+        self.__selections = ['PICTURES', 'MUSIC', 'VIDEOS']
+        self.currentSelection = QLabel()
+        self.btnGroup = self.__homeMenu.getButtonGroup()
         self.__myHomeWidget = QWidget()
 
     def __initUI(self):
         self.setWindowTitle("Pi Media Center")
         self.setFixedSize(QSize(1080, 720))
-
+      
         #Welcome Message
         self.__welcomeMessage.setText("Welcome to Pi Media Center")
-        self.__welcomeMessage.setFont(QFont("Sanserif", 15))
+        self.__welcomeMessage.setFont(QFont("Goudy Old Style", 20))
         self.__welcomeMessage.setAlignment(Qt.AlignCenter|Qt.AlignTop)
-  
+
         #Menu
         self.__loadMenu()
         self.__homeMenu.setFixedSize(500, 500)
 
+        #Current Selection
+        self.btnGroup.idToggled.connect(self.printCurrentSelection)
+        self.currentSelection.setText(self.__selections[0])
+        self.currentSelection.setFont(QFont("Consolas", 10))
+        self.currentSelection.setAlignment(Qt.AlignCenter|Qt.AlignTop)
+
         #layout
         layout = QVBoxLayout()
-        layout.setContentsMargins(0, 100, 0, 0)
+        layout.setContentsMargins(0, 50, 0, 0)
         layout.setSpacing(0)
         layout.addWidget(self.__welcomeMessage)
         layout.addWidget(self.__homeMenu, alignment= Qt.AlignHCenter| Qt.AlignVCenter)
+        layout.addWidget(self.currentSelection, stretch= -1000)
         layout.insertStretch( -1, 1 )
 
         self.__myHomeWidget.setLayout(layout)
+        self.setStyleSheet('border: solid 2px')
         self.setCentralWidget(self.__myHomeWidget)
     
     def __loadMenu(self):
@@ -56,6 +67,12 @@ class HomeScreen(QMainWindow):
         prevBtn.setFixedSize(60, 60)
         nextBtn = self.__homeMenu.getNextBtn()
         nextBtn.setFixedSize(60, 60)
+    
+    def printCurrentSelection(self):
+        idx = self.btnGroup.checkedId()
+        self.currentSelection.setText(self.__selections[idx])
+        
+  
 
 
        
