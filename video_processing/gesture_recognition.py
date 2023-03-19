02c3772b4e31a781129eb25716887434698bcee3
Kuramenai <marcelynsteeve@gmail.com>
2023-03-19 15:47:57 +0800

    Add method to preprocess the detected landmarks

--- video_processing/gesture_recognition.py
+++ video_processing/gesture_recognition.py
@@ -1,5 +1,9 @@
+import copy
+
 import cv2
 import time
+import numpy as np
+import itertools
 from video_stream import VideoStream
 from hand_tracking_module import HandDetector
 
@@ -22,8 +26,7 @@ class GestureRecognition:
                 frame = self.__detector.find_hands(frame, draw=True)
                 landmarks = self.__detector.find_landmarks(frame, draw=True)
 
-                if len(landmarks) != 0:
-                    pass
+                self.get_processed_landmarks(landmarks)
 
                 self.__fps = self.get_fps()
                 # Display the fps value on the screen
@@ -40,7 +43,40 @@ class GestureRecognition:
 
         cv2.destroyAllWindows()
 
+    def get_processed_landmarks(self, landmarks: list):
+        """Calculate the positions of the landmarks relative to the wrist"""
+
+        # Copy the landmarks list to prevent unwanted changes
+        landmarks_copy = copy.deepcopy(landmarks)
+
+        # If wrist is detected calculate the other landmark point positions relative to it
+        if landmarks_copy:
+            if landmarks[0]:
+                wrist_xpos, wrist_ypos = landmarks[0][1], landmarks[0][2]
+                for index, landmark_point in enumerate(landmarks_copy):
+                    landmarks_copy[index][0] -= wrist_xpos
+                    landmarks_copy[index][1] -= wrist_ypos
+
+                landmarks_copy = list(itertools.chain.from_iterable(landmarks_copy))
+
+                # Find the greatest landmark point
+                max_value = max(list(map(abs, landmarks_copy)))
+                # Normalization
+                landmarks_copy = np.copy(landmarks_copy)
+                landmarks_copy = landmarks_copy/max_value
+
+        return landmarks_copy
+
+
+
+
+
+
+
+
+
     def get_fps(self):
+        """Calculate the frame rate of the pipeline"""
         self.__current_time = time.time()
         fps = 1 / (self.__current_time - self.__previous_time)
         self.__previous_time = self.__current_time
